bd8495a10f6cc52f1a7e55758240338ed366c333
cse19922021 <95384987+cse19922021@users.noreply.github.com>
2022-02-01 09:58:20 -0500

    Add files via upload

---
+++ figure9.py
@@ -0,0 +1,109 @@
+from numpy.core.fromnumeric import resize, shape
+import plotly.express as px
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+
+def load_csv(file_path):
+    return pd.read_csv(file_path, sep=',', header=0)
+
+class Figures():
+    def __init__(self, data) -> None:
+        self.data = data
+        self.cmin = -0.1
+        self.cmax = 2.5
+
+        self.adjust_layout()
+        self.rootCause_to_symptom()
+
+    def adjust_layout(self):
+        df = [self.data['Root cause High level'] , self.data['Fixing pattern level 2']]
+        
+        result = pd.concat(df, axis=1)
+
+        categorical_dimensions = ['Root cause High level', 'Fixing pattern level 2']
+        dimensions = [dict(values=result[label], label=label) for label in categorical_dimensions]
+
+        dimensions[0]['label'] = 'Root Causes'
+        dimensions[1]['label'] =  'Fixing Patterns'
+
+        result["Fixing pattern level 2"] = result["Fixing pattern level 2"]\
+            .map({'Add Checker for Null Pointer': 1, 'Add Checker for Overflow': 2,\
+            'Add Checker for Recurrsion': 3, 'Add Checker for Tensors Property': 4, \
+            'Add Locking Mechanism': 5, 'Avoid Stack Overflow on Deep Graphs': 6, \
+            'Close File Handler to Prevent File Leak': 7, 'Convert Integer Sign': 8, 'Handle Numerical Precision Error': 9,\
+            'Improve Exception Handling': 10,\
+            'Increase Integer Type Range': 11,\
+            'Initializae Resource': 12,\
+            'Memory Release Management': 13,\
+            'Modify Data Type': 14,\
+            'Modify Function Return Value': 15,\
+            'Modify Index Calculation': 16,\
+            'Modify Locking Mechanism': 17,\
+            'Modify Order of Execution': 18,\
+            'Others': 19,\
+            'Remove Locking Mechansim': 20,\
+            'Update API Usage': 21,\
+            'Update API Version': 22,\
+            'Use Proper API': 23})
+
+
+        color = result['Fixing pattern level 2'].values
+
+        colorscale = [[0, 'blue'], [0.33, 'red'], [0.33, 'aliceblue'], [0.66, 'green'], [0.66, 'black'], [1.0, 'darkcyan']]
+        layout = go.Layout(
+            autosize=False,
+            width=1400,
+            height=600,
+
+            xaxis= go.layout.XAxis(linecolor = 'red',
+                                linewidth = 10,
+                                mirror = True),
+
+            yaxis= go.layout.YAxis(linecolor = 'black',
+                                linewidth = 10,
+                                mirror = True),
+
+            margin=go.layout.Margin(
+                l=300,
+                r=500,
+                b=100,
+                t=100,
+                pad = 8
+            )
+        )
+
+        trace1 = go.Parcats(dimensions=dimensions,
+        line={'colorscale': colorscale, 'cmin': self.cmin,'cmax': self.cmax, 'color': color, 'shape': 'hspline'})
+        data = [trace1]
+
+        fig = go.Figure(data=data, layout=layout)
+        return fig, result, color, colorscale
+
+    def rootCause_to_symptom(self):
+        fig, result, color, colorscale = self.adjust_layout()
+        colors = {
+                'background': 'white',
+                'text': 'black'
+            }
+        fig.update_layout(
+        plot_bgcolor=colors['background'],
+        paper_bgcolor=colors['background'],
+        font_color=colors['text'],
+        font_size=15
+        )
+
+        fig.show()
+        fig.write_html("./fixingpatterns.html")
+
+     
+
+
+def main():
+    file_path = './data.csv'
+    data = load_csv(file_path)
+    Figures(data)
+    
+if __name__ == '__main__':
+    main()
